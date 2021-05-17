216c91499ac93ec3ef7cae9eb0388b520e7874c9
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-05-17 16:13:48 +0100

    hackerrank SQL solution PADS

---
+++ hackerrank/SQL/02.Advanced Select/002.PADS.sql
@@ -0,0 +1,2 @@
+select concat(name, '(', substring(occupation,1,1), ')') from occupations order by name asc;
+select concat('There are a total of ',count(occupation),' ',lower(occupation), 's.') from occupations group by occupation order by count(occupation);
