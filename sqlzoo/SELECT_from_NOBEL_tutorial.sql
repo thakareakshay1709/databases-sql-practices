e669e46b548fd4caa7f15a83ad615df3790a4b95
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-08-23 22:13:10 +0100

    sqlzoo solution for select from nobel tutorial

--- sqlzoo/SELECT_from_NOBEL_tutorial.sql
+++ sqlzoo/SELECT_from_NOBEL_tutorial.sql
@@ -25,3 +25,23 @@ SELECT YR, SUBJECT, WINNER
 FROM NOBEL
 WHERE SUBJECT = 'Literature'
 AND YR >= 1980 AND YR <=1989;
+
+--6. Show all details of the presidential winners:
+--Theodore Roosevelt
+--Woodrow Wilson
+--Jimmy Carter
+--Barack Obama
+
+SELECT * FROM NOBEL
+WHERE WINNER IN ('Theodore Roosevelt', 'Woodrow Wilson', 'Jimmy Carter', 'Barack Obama');
+
+--7. Show the winners with first name John
+SELECT WINNER FROM NOBEL
+WHERE WINNER LIKE 'John%';
+
+--8. Show the year, subject, and name of Physics winners for 1980 together with the Chemistry winners for 1984.
+SELECT YR, SUBJECT, WINNER
+FROM NOBEL
+WHERE (YR = '1980' AND SUBJECT = 'Physics') 
+OR
+(YR = '1984' AND SUBJECT = 'Chemistry');
