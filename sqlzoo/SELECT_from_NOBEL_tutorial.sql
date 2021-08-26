932930d500b65ea004ab5534053fbeba5fed9de3
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-08-26 23:39:32 +0100

    SQLZoo solution for select from NOBEL tutorial

--- sqlzoo/SELECT_from_NOBEL_tutorial.sql
+++ sqlzoo/SELECT_from_NOBEL_tutorial.sql
@@ -80,7 +80,7 @@ WHERE WINNER LIKE 'Sir%'
 ORDER BY YR DESC, WINNER ASC;
 
 --14. The expression subject IN ('Chemistry','Physics') can be used as a value - it will be 0 or 1.
---Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last.
+--Show the 1984 winners and subject ordered by subject and winner name; but list Chemistry and Physics last
 
 SELECT WINNER, SUBJECT
 FROM NOBEL
