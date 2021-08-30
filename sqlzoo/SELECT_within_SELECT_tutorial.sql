85a5a787b71997357b456d5bfd83783bdeea5b6b
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-08-30 23:22:29 +0100

    sqlzoo solution for 2nd problem in select within select tut

--- sqlzoo/SELECT_within_SELECT_tutorial.sql
+++ sqlzoo/SELECT_within_SELECT_tutorial.sql
@@ -1,3 +1,7 @@
 -- 1. List each country name where the population is larger than that of 'Russia'.
 SELECT name FROM world 
 WHERE population > (SELECT population FROM world WHERE name='Russia');
+
+--2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
+select name from world where 
+gdp/population > (select gdp/population from world where name ='United Kingdom') and continent like 'Europe';
