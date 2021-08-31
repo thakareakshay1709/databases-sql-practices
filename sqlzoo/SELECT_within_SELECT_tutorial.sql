121c3771ed41b257eaa3f0cab0f9f85f5b6a30fe
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-08-31 15:18:35 +0100

    solution for 3,4 problem for sqlzoo select within select tutorial

--- sqlzoo/SELECT_within_SELECT_tutorial.sql
+++ sqlzoo/SELECT_within_SELECT_tutorial.sql
@@ -5,3 +5,19 @@ WHERE population > (SELECT population FROM world WHERE name='Russia');
 --2. Show the countries in Europe with a per capita GDP greater than 'United Kingdom'.
 select name from world where 
 gdp/population > (select gdp/population from world where name ='United Kingdom') and continent like 'Europe';
+
+--3. List the name and continent of countries in the continents containing either Argentina or Australia. Order by name of the country.
+SELECT NAME, CONTINENT FROM world
+WHERE CONTINENT IN 
+(SELECT CONTINENT FROM world WHERE NAME IN ('Argentina', 'Australia'))
+ORDER BY NAME;
+
+--4. Which country has a population that is more than Canada but less than Poland? Show the name and the population.
+SELECT NAME, POPULATION FROM world
+WHERE POPULATION > 
+(SELECT POPULATION FROM world 
+WHERE NAME = 'Canada')
+AND
+POPULATION < 
+(SELECT POPULATION FROM world 
+WHERE NAME = 'Poland');
