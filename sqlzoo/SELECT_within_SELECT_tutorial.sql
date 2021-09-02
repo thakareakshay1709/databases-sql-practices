ee176ad3bade9477468bc1449c48e7b5239c5afa
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-09-02 21:48:12 +0100

    sqlzoo solution for select within select tutorial

--- sqlzoo/SELECT_within_SELECT_tutorial.sql
+++ sqlzoo/SELECT_within_SELECT_tutorial.sql
@@ -25,6 +25,7 @@ WHERE NAME = 'Poland');
 --5. Germany (population 80 million) has the largest population of the countries in Europe. Austria (population 8.5 million) has 11% of the population of Germany.
 --Show the name and the population of each country in Europe. Show the population as a percentage of the population of Germany.
 
+
 SELECT NAME, CONCAT(ROUND((POPULATION*100)/(SELECT POPULATION FROM world WHERE NAME = 'Germany'),0),'%') AS percentage FROM world
 WHERE POPULATION in (
 SELECT POPULATION FROM world
