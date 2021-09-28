9b1c87b176c91ec725a56404d798e19528b19739
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-09-28 11:17:17 +0100

    adding sqlzoo solution for join operation

--- sqlzoo/The_Join_Operation.sql
+++ sqlzoo/The_Join_Operation.sql
@@ -10,3 +10,24 @@ WHERE teamid ='GER';
 SELECT id,stadium,team1,team2
 FROM game where id = (select matchid from goal where teamid='GER'
 and player = 'Lars Bender');
+
+--3.You can combine the two steps into a single query with a JOIN.
+--SELECT *
+--FROM game JOIN goal ON (id=matchid)
+--The FROM clause says to merge data from the goal table with that from the game table. The ON says how to figure out which rows in game go with which rows in goal - the matchid from goal must match id from game. (If we wanted to --be more clear/specific we could say
+--ON (game.id=goal.matchid)
+
+--The code below shows the player (from the goal) and stadium name (from the game table) for every goal scored.
+
+--Modify it to show the player, teamid, stadium and mdate for every German goal.
+
+SELECT player,teamid,stadium,mdate
+FROM game JOIN goal ON (id=matchid)
+where teamid ='GER';
+
+-- 4. Use the same JOIN as in the previous question.
+--Show the team1, team2 and player for every goal scored by a player called Mario player LIKE 'Mario%'
+
+select team1, team2, player from goal
+JOIN game ON (matchid=id)
+where player like 'Mario%';
