f65e1dca09899709ad40c4e8432fa26f3047ef5d
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-09-29 17:35:11 +0100

    adding sqlzoo solution for join operation

--- sqlzoo/The_Join_Operation.sql
+++ sqlzoo/The_Join_Operation.sql
@@ -31,3 +31,12 @@ where teamid ='GER';
 select team1, team2, player from goal
 JOIN game ON (matchid=id)
 where player like 'Mario%';
+
+-- 5 The table eteam gives details of every national team including the coach. You can JOIN goal to eteam using the phrase goal JOIN eteam on teamid=id
+
+-- Show player, teamid, coach, gtime for all goals scored in the first 10 minutes gtime<=10
+
+SELECT player, teamid, coach, gtime
+FROM goal 
+JOIN eteam ON (teamid=id)
+WHERE gtime<=10;
