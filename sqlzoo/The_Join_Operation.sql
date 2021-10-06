5a03f6d8a2ed17bb585b75f21f2aba1bc3ef229c
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-10-06 23:08:26 +0100

    adding sqlzoo solution for join operations

--- sqlzoo/The_Join_Operation.sql
+++ sqlzoo/The_Join_Operation.sql
@@ -84,3 +84,5 @@ FROM game
 JOIN goal ON goal.matchid = game.id
 WHERE (team1 = 'POL' OR team2 = 'POL')
 GROUP BY goal.matchid
+
+--12. For every match where 'GER' scored, show matchid, match date and the number of goals scored by 'GER'
