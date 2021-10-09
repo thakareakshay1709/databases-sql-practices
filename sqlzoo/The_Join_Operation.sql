d4a40a1818aa210a3a8a588492fcc2f99b2d0883
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-10-09 21:17:43 +0100

    adding sqlzoo solution for join operation

--- sqlzoo/The_Join_Operation.sql
+++ sqlzoo/The_Join_Operation.sql
@@ -92,3 +92,14 @@ from goal
 join game on matchid=id
 where teamid = 'GER'
 group by matchid,mdate;
+
+--13. List every match with the goals scored by each team as shown. This will use "CASE WHEN" which has not been explained in any previous exercises.
+
+
+SELECT mdate,
+  team1,
+  SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) score1,
+  team2,
+  SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) score2
+  FROM game LEFT JOIN goal ON matchid = id 
+GROUP BY mdate,matchid,team1,team2
