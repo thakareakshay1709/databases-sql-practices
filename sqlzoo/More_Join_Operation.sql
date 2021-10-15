9bf3a94ef4b7641a2a9a0a1e26fe43d909c58223
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2021-10-15 16:46:28 +0100

    adding sqlzoo solution for more join

--- sqlzoo/More_Join_Operation.sql
+++ sqlzoo/More_Join_Operation.sql
@@ -5,3 +5,8 @@ WHERE yr=1962;
 
 --2. Give year of 'Citizen Kane'.
 select yr from movie where title = 'Citizen Kane';
+
+--3. List all of the Star Trek movies, include the id, title and yr (all of these movies include the words Star Trek in the title). Order results by year.
+select id, title, yr from movie
+where title like '%Star Trek%'
+order by yr;
