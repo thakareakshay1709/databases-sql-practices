86f1c64d980c8b87d717bede57bb3ad3b9b2d1ae
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-01-01 22:54:41 +0000

    adding sqlzoo solution for using null operation

--- sqlzoo/USING_NULL.sql
+++ sqlzoo/USING_NULL.sql
@@ -8,3 +8,8 @@ select NAME from teacher where dept IS NULL;
 
 --2. Note the INNER JOIN misses the teachers with no department and the departments with no teacher.
 SELECT teacher.name, dept.name FROM teacher INNER JOIN dept ON (teacher.dept=dept.id);
+
+--3. Use a different JOIN so that all teachers are listed.
+select teacher.name, dept.name from dept 
+right join teacher
+on (teacher.dept=dept.id);
