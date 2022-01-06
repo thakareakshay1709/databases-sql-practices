6eb06da6d2cf67e95d097497d9e8870c92f9d2e3
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-01-06 23:19:49 +0000

    adding sqlzoo solution for using null operations

--- sqlzoo/USING_NULL.sql
+++ sqlzoo/USING_NULL.sql
@@ -20,3 +20,7 @@ right join dept on (dept.id=teacher.dept);
 
 --5. Use COALESCE to print the mobile number. Use the number '07986 444 2266' if there is no number given. Show teacher name and mobile number or '07986 444 2266'
 SELECT name, COALESCE(mobile, '07986 444 2266') from teacher;
+
+--6. Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. Use the string 'None' where there is no department.
+select teacher.name, COALESCE(dept.name, 'None') from teacher
+left join dept on (dept.id=teacher.dept);
