6f2a93c74f94f0a183256f81729a094da6132e78
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-01-10 23:14:03 +0000

    adding sqlzoo solution for using null operation

--- sqlzoo/USING_NULL.sql
+++ sqlzoo/USING_NULL.sql
@@ -41,3 +41,12 @@ then 'Sci'
 else 'Art'
 end
 from teacher;
+
+--10. Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+select name,
+case 
+when dept in (1,2) then 'Sci'
+when dept = 3 then 'Art'
+else 'None'
+end
+from teacher;
