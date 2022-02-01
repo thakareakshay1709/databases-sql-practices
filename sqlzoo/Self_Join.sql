ffd916c2338482d1974ea59c9420d31986032cc7
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-02-01 22:23:15 +0000

    adding sqlzoo solution for self join

--- sqlzoo/Self_Join.sql
+++ sqlzoo/Self_Join.sql
@@ -1,3 +1,6 @@
 -- Self Join
 --1. How many stops are in the database.
-select count(*) from stops
+select count(*) from stops;
+
+--2. Find the id value for the stop 'Craiglockhart'
+select id from stops where name ='Craiglockhart';
