3eda5864746e00f5bfd957f0a2f8555056da1be1
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-02-02 10:00:57 +0000

    adding sqlzoo solution for self join

--- sqlzoo/Self_Join.sql
+++ sqlzoo/Self_Join.sql
@@ -4,3 +4,9 @@ select count(*) from stops;
 
 --2. Find the id value for the stop 'Craiglockhart'
 select id from stops where name ='Craiglockhart';
+
+--3. Give the id and the name for the stops on the '4' 'LRT' service.
+select id, name from stops where id in (
+select stop from route where num = '4'
+and
+company = 'LRT');
