d750c49c1503c2e3773b19394b55ddac31b28eca
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-02-07 23:42:21 +0000

    adding solution for self join sqlzoo

--- sqlzoo/Self_Join.sql
+++ sqlzoo/Self_Join.sql
@@ -10,3 +10,7 @@ select id, name from stops where id in (
 select stop from route where num = '4'
 and
 company = 'LRT');
+
+--4. The query shown gives the number of routes that visit either London Road (149) or Craiglockhart (53). 
+-- Run the query and notice the two services that link these stops have a count of 2. 
+-- Add a HAVING clause to restrict the output to these two routes.
