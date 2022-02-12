9a5499f5e14fb5f82b7f198d91b392bb288764fb
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-02-12 22:30:50 +0000

    adding sqlzoo solution for self join operation

--- sqlzoo/Self_Join.sql
+++ sqlzoo/Self_Join.sql
@@ -20,3 +20,7 @@ GROUP BY company, num
 HAVING COUNT(*) =2;
 
 --5. Execute the self join shown and observe that b.stop gives all the places you can get to from Craiglockhart, without changing routes. Change the query so that -- it shows the services from Craiglockhart to London Road.
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53;
