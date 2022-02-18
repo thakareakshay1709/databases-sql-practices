06f9ffafe0d069c016bd726a719f7a4e0488bf88
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-02-18 23:43:39 +0000

    adding sqlzoo solution

--- sqlzoo/Self_Join.sql
+++ sqlzoo/Self_Join.sql
@@ -24,3 +24,6 @@ SELECT a.company, a.num, a.stop, b.stop
 FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
 WHERE a.stop=53 and b.stop=149;
+
+--6. The query shown is similar to the previous one, however by joining two copies of the stops table we can refer to stops by name rather than by number. Change the query so that the services between 'Craiglockhart' and 'London Road' are shown. If you are tired of these places try 'Fairmilehead' against 'Tollcross'
+
