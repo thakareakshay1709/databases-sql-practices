f6d3702c3735a7979d7144fced8d327690f471a8
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2022-02-23 22:43:08 +0000

    sqlzoo

--- sqlzoo/Self_Join.sql
+++ sqlzoo/Self_Join.sql
@@ -32,4 +32,4 @@ FROM route a JOIN route b ON
   (a.company=b.company AND a.num=b.num)
   JOIN stops stopa ON (a.stop=stopa.id)
   JOIN stops stopb ON (b.stop=stopb.id)
-WHERE stopa.name='Craiglockhart'
+WHERE stopa.name='Craiglockhart';
