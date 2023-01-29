6cb431c92beb0e7b2269558d6997c5a9b24eb884
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2023-01-29 20:18:51 +0000

    Update SELECT_from_WORLD_tutorial.sql

--- sqlzoo/SELECT_from_WORLD_tutorial.sql
+++ sqlzoo/SELECT_from_WORLD_tutorial.sql
@@ -1,4 +1,4 @@
--- 1. Observe the result of running this SQL command to show the name, continent and population of all countries.
+-- 1. Observe the res of running this SQL command to show the name, continent and population of all countries.
 SELECT name, continent, population FROM world;
 
 -- 2. Show the name for the countries that have a population of at least 200 million. 200 million is 200000000, there are eight zeros.
