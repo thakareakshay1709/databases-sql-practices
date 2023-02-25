747c749b16157c2635d9f15c35744490648026fa
Akshay Thakare <57269333+thakareakshay1709@users.noreply.github.com>
2023-02-25 23:37:06 +0000

    Update SELECT_within_SELECT_tutorial.sql

--- sqlzoo/SELECT_within_SELECT_tutorial.sql
+++ sqlzoo/SELECT_within_SELECT_tutorial.sql
@@ -1,4 +1,4 @@
---1. List each cntry name where the population is larger than 'Russia'.
+--1. List each country name where the population is larger than 'Russia'.
 SELECT name FROM world 
 WHERE population > (SELECT population FROM world WHERE name='Russia');
 
